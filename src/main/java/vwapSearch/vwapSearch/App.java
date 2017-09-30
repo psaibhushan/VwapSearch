a0182a80ed52b378c921aa5efefd0b8013563430
Sai Bhushan <saibhushan.p@olacabs.com>
2017-09-30 14:47:01 +0530

    Initial commit

---
+++ src/main/java/vwapSearch/vwapSearch/App.java
@@ -0,0 +1,13 @@
+package vwapSearch.vwapSearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
